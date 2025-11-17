9b83e894982dd366875ac6b75cee2f875297c7cb
Beatriz Xavier <bixavier06@gmail.com>
2025-11-16 21:27:13 -0300

    Implement list operations and print examples
    Add basic list operations and print statements in Listas_2.py

---
+++ Listas_2.py
@@ -0,0 +1,9 @@
+x=[3,4,5,6,67]
+print('x[0]='+ str(x[0]))                     # O que estas nas aspas copia
+print('x[1]='+ str(x[2]))                     # É (o + str) puxa a respota do X
+print(x[-2])                                  # Se o numero for negativo a incógnita da lista será decrescente
+print(x[3])                                   # Len = fornece o número da lista
+print(x[-4])                                  # -3 significa a posição
+print("len(x)=" + str(len(x)) )               # para colocar mais de um numero tem que colocar ":"
+print(x[len(x)-3])
+print('x[:4]=' + str(x[:4]))
