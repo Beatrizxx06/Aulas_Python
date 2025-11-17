a586eb83662c2e4d3e03a6a22ae9992cb858595d
Beatriz Xavier <bixavier06@gmail.com>
2025-11-16 21:25:49 -0300

    Calculate area and double of a square
    Fix calculation of area and double value.

---
+++ Area_de_um_Quadrado.py
@@ -0,0 +1,6 @@
+# vai caculas a área de um trabalho e depois vai mosta o dobro
+somma = quadrado = 0
+quadrado = float(input(f'informe a medida de um lado de um quadrado:'))
+área = quadrado * 4
+dobro = área * 2
+print(f'O dobrp da área {área} do quadrado {dobro}')
