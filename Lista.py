81548e6371d46a01ad679f4a0b9ff90217361cd5
Beatriz Xavier <bixavier06@gmail.com>
2025-11-16 21:26:40 -0300

    Add initial lists and print statements in Lista.py

---
+++ Lista.py
@@ -0,0 +1,6 @@
+x=[1,2,3,4,5,6]                             #Lista de valores tipo Int
+y=['a,b,c,d,e,f']                            #Lista de valores tipo str
+z=[1.2,1.31,1.4]                            #Lista de valores tipo float
+print(x)
+print(y)
+print(z)
